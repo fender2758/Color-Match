7c293e394be8b8f1c924cc566b4331ecfc5897de
Babamvirus <kkyyhh618@gmail.com>
2021-05-26 23:23:24 +0900

    서브그룹 폴더 생성. 씬/스크립트 폴더와 데모 apk 파일 추가

---
+++ SubGroup1/Scripts/Quiz.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    Color textColor, edgeColor;
+    public Color TextColor
+    {
+        get { return textColor; }
+        set
+        {
+            textColor = value;
+            text.color = value;
+        }
+    }
+    public Color EdgeColor
+    {
+        get { return edgeColor; }
+        set
+        {
+            edgeColor = value;
+            outer.color = value;
+        }
+    }
+    public Image outer, inner;
+    public TextMeshProUGUI text;
+
+    /** wordString is same as answer.*/
+    string wordString;
+    public string WordString
+    {
+        get
+        {
+            return wordString;
+        }
+        set
+        {
+            wordString = value;
+            text.text = wordString;
+        }
+    }
+}
